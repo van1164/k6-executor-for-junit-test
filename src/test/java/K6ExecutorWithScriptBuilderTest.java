9a8dfd1f6bb13c708152f3d9eb6913329a2d12bc
Sihwan Kim <vkn123@naver.com>
2024-10-20 23:44:08 +0900

    test: scriptBUilderExecutor test

--- src/test/java/K6ExecutorWithScriptBuilderTest.java
+++ src/test/java/K6ExecutorWithScriptBuilderTest.java
@@ -1,6 +1,12 @@
 import com.sun.net.httpserver.HttpServer;
+import io.github.van1164.executor.K6ExecutorWithScriptBuilder;
 import io.github.van1164.executor.K6ExecutorWithScriptPath;
 import io.github.van1164.result.K6Result;
+import io.github.van1164.scirptBuilder.Check;
+import io.github.van1164.scirptBuilder.HttpMethod;
+import io.github.van1164.scirptBuilder.HttpRequest;
+import io.github.van1164.scirptBuilder.K6ScriptBuilder;
+import io.github.van1164.util.K6Imports;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,10 +18,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class K6ExecutorWithScriptPathTest {
+public class K6ExecutorWithScriptBuilderTest {
     static HttpServer server;
 
-    public K6ExecutorWithScriptPathTest() throws IOException {
+    public K6ExecutorWithScriptBuilderTest() throws IOException {
     }
 
     @BeforeAll
@@ -30,94 +36,29 @@ public class K6ExecutorWithScriptPathTest {
 
     @Test
     public void isAllPassedTest() throws IOException {
-        List<String> checkList = List.of("is status 200", "response time < 500ms");
-        K6ExecutorWithScriptPath executor = K6ExecutorWithScriptPath.builder().scriptPath("test.js").checkList(checkList).build();
 
-        K6Result result = executor.runTest();
-        assertTrue(result.isAllPassed(), "K6 load test failed");
-
-    }
-
-    @Test
-    public void isAllPassedListTest() throws IOException {
-        List<String> checkList = List.of("is status 200", "response time < 500ms");
-        K6ExecutorWithScriptPath executor = K6ExecutorWithScriptPath.builder().scriptPath("test.js").checkList(checkList).build();
-        K6Result result = executor.runTest();
-        assertTrue(result.isAllPassed(), "K6 load test failed");
-
-    }
-
-    @Test
-    public void printResultTest() throws IOException {
-        List<String> checkList = List.of("is status 200", "response time < 500ms");
-        K6ExecutorWithScriptPath executor = K6ExecutorWithScriptPath.builder().scriptPath("test.js").checkList(checkList).build();
-
-        K6Result result = executor.runTest();
-        result.printResult();
-
-    }
-
-    @Test
-    public void getFailedCheckListTest() throws Exception {
-        server.stop(0);
-        List<String> checkList = List.of("is status 200", "response time < 500ms");
-        K6ExecutorWithScriptPath executor = K6ExecutorWithScriptPath.builder().scriptPath("test.js").checkList(checkList).build();
-        K6Result result = executor.runTest();
-        assertEquals(result.getFailedCheckList(), List.of("is status 200"));
-
-        serverStart();
-    }
-
-    @Test
-    public void getRequestCountTest() throws IOException {
-        List<String> checkList = List.of("is status 200", "response time < 500ms");
-        K6ExecutorWithScriptPath executor = K6ExecutorWithScriptPath.builder().scriptPath("test.js").checkList(checkList).build();
-
-        K6Result result = executor.runTest();
-        assertTrue(result.httpRequestFound());
-        assertEquals(result.getTotalRequest(), result.getSuccessRequest() + result.getFailRequest());
-
-    }
-
-    @Test
-    public void getCounterTest() throws IOException {
-        List<String> checkList = List.of("is status 200", "response time < 500ms");
-        List<String> counterList = List.of("success_check");
-        K6ExecutorWithScriptPath executor = K6ExecutorWithScriptPath.builder()
-                .scriptPath("counter_test.js")
-                .checkList(checkList)
-                .counterList(counterList)
+        HttpRequest request = new HttpRequest.Builder("res")
+                .method(HttpMethod.GET)
+                .url("http://localhost:8080")
                 .build();
 
-        K6Result result = executor.runTest();
-        assertTrue(result.httpRequestFound());
-        assertEquals(result.getTotalRequest(), result.getSuccessRequest() + result.getFailRequest());
-        result.printResult();
-        assertEquals(result.getCount("success_check"), result.getSuccessRequest());
+        Check statusCheck = Check.statusCheck("res", 200);
+        Check responseTimeCheck = new Check("res", "response time < 500ms","(r) => r.timings.duration < 50000");
+        // Act
+        K6ScriptBuilder script = new K6ScriptBuilder()
+                .addImport("import http from 'k6/http'")
+                .addImport(K6Imports.Check)
+                .addHttpRequest(request)
+                .addCheck(statusCheck)
+                .addCheck(responseTimeCheck);
 
-    }
+        K6ExecutorWithScriptBuilder executor = K6ExecutorWithScriptBuilder.builder().sb(script).build();
 
-    @Test
-    public void getArgsTest() throws IOException {
-        List<String> checkList = List.of("is status 200", "response time < 500ms");
-        List<String> counterList = List.of("args_counterA", "args_counterB");
-        HashMap<String, String> args = new HashMap<>();
-        args.put("TESTA", "abc");
-        args.put("TESTB", "def");
-        K6ExecutorWithScriptPath executor = K6ExecutorWithScriptPath.builder()
-                .scriptPath("args_test.js")
-                .checkList(checkList)
-                .counterList(counterList)
-                .args(args)
-                .build();
+        System.out.println(script.build());
 
         K6Result result = executor.runTest();
-        assertTrue(result.httpRequestFound());
-        assertEquals(result.getTotalRequest(), result.getSuccessRequest() + result.getFailRequest());
-        assertNotNull(result.getCount("args_counterA"));
-        assertNotNull(result.getCount("args_counterB"));
-        assertEquals(result.getCount("args_counterA"), result.getCount("args_counterB"));
         result.printResult();
+        assertTrue(result.isAllPassed(), "K6 load test failed");
 
     }
 
